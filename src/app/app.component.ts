bea6e68af63881a7e1508f7e099e55acc884cc96
EmpiricalCode <daniellongschool@gmail.com>
2023-07-19 10:41:05 -0400

    Added logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,9 @@ export class AppComponent {
   constructor(private authenticationService: AuthenticationService) { }
   
   loggedIn: Observable<boolean> = this.authenticationService.loggedIn;
+
+  logout() {
+    localStorage.removeItem("token");
+    window.location.reload();
+  }
 }
